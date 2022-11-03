e7cef7310db63f019545875d2c69445bd9f95740
FillipeEduardo <fillipe_oliveira@outlook.com.br>
2022-11-03 18:09:50 -0300

    requisito quatro

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Carregando from '../Carregando';
 
@@ -33,6 +34,9 @@ export default class Header extends Component {
       >
         { isLoading ? <Carregando /> : (
           <span data-testid="header-user-name">{ name }</span>) }
+        <Link data-testid="link-to-search" to="/search">Pesquisa</Link>
+        <Link data-testid="link-to-favorites" to="/favorites">Favoritas</Link>
+        <Link data-testid="link-to-profile" to="/profile">Favoritas</Link>
       </header>
     );
   }
