42bd188321e86744045881efb6f62eaad2f97abf
FillipeEduardo <fillipe_oliveira@outlook.com.br>
2022-11-04 11:48:02 -0300

    requisito 12

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import MusicCard from '../components/MusicCard';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 export default class Favorites extends Component {
+  state = {
+    favoritas: [],
+  };
+
+  async componentDidMount() {
+    const favoritas = await getFavoriteSongs();
+    this.setState({
+      favoritas,
+    });
+  }
+
+  async componentDidUpdate() {
+    const favoritasAtualizadas = await getFavoriteSongs();
+    this.setState({
+      favoritas: favoritasAtualizadas,
+    });
+  }
+
   render() {
+    const { favoritas } = this.state;
     return (
       <div data-testid="page-favorites">
         <Header />
+        {
+          favoritas.map((favorita) => (
+            <MusicCard
+              key={ favorita.trackId }
+              trackName={ favorita.trackName }
+              previewUrl={ favorita.previewUrl }
+              trackId={ favorita.trackId }
+              musica={ favorita }
+              favorita
+            />
+          ))
+        }
       </div>
     );
   }
